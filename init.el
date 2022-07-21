c0469fb9af81168e78d410ebfd76bc6c6063103c
JackMoriarty <chenbangduo@qq.com>
2022-07-22 00:48:55 +0800

    update

--- init.el
+++ init.el
@@ -147,7 +147,7 @@
   :config
   (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
   (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
-  (setq lsp-ui-doc-position 'top))
+  (setq lsp-ui-doc-position 'at-point))
 (use-package lsp-ivy
   :ensure t
   :after (lsp-mode))
