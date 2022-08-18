83388ed1fb0f9f8b09a04c9bb29e2f57c10d74b7
JackMoriarty <chenbangduo@qq.com>
2022-08-18 23:05:51 +0800

    change to atom one dark theme

--- init.el
+++ init.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "171d1ae90e46978eb9c342be6658d937a83aaa45997b1d7af7657546cae5985b" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default))
+   '("24168c7e083ca0bbc87c68d3139ef39f072488703dcdd82343b8cab71c0f62a7" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "171d1ae90e46978eb9c342be6658d937a83aaa45997b1d7af7657546cae5985b" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default))
  '(package-selected-packages
    '(cuda-mode monokai-pro-theme atom-one-dark-theme highlight-indentation pyvenv magit vterm monokai-theme undo-tree lsp-treemacs treemacs-projectile treemacs lsp-pyright counsel-projectile projectile lsp-ivy lsp-ui lsp-mode flycheck company-tabnine company rainbow-delimiters highlight-symbol dashboard marginalia which-key good-scroll smart-mode-line mwim ace-window amx counsel use-package)))
 (custom-set-faces
@@ -26,11 +26,11 @@
 (eval-when-compile
   (require 'use-package))
 
-; 安装monokai主题
-(use-package monokai-pro-theme
+; 安装atom-one-dark主题
+(use-package atom-one-dark-theme
   :ensure t
   :init
-  (load-theme 'monokai-pro 1))
+  (load-theme 'atom-one-dark 1))
 
 ; 安装smart-mode-line
 (use-package smart-mode-line
