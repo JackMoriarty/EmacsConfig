626dc754f8f19694ec2b9e8b9f308953c4b0921b
JackMoriarty <chenbangduo@qq.com>
2022-08-20 14:50:15 +0800

    update treemacs.

--- init.el
+++ init.el
@@ -230,7 +230,7 @@
         ("C-x t 1"   . treemacs-delete-other-windows)
         ("C-x t t"   . treemacs)
         ("C-x t B"   . treemacs-bookmark)
-        ;; ("C-x t C-t" . treemacs-find-file)
+        ("C-x t C-t" . treemacs-find-file)
         ("C-x t M-t" . treemacs-find-tag))
   (:map treemacs-mode-map
 	("/" . treemacs-advanced-helpful-hydra)))
