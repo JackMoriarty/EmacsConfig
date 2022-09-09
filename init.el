32972d55639b08f7492e96dd121773234d9d5718
chenbangduo.cbd <chenbangduo.cbd@alibaba-inc.com>
2022-09-09 10:25:00 +0800

    change to monokai-pro theme

--- init.el
+++ init.el
@@ -6,7 +6,8 @@
  '(custom-safe-themes
    '("24168c7e083ca0bbc87c68d3139ef39f072488703dcdd82343b8cab71c0f62a7" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "171d1ae90e46978eb9c342be6658d937a83aaa45997b1d7af7657546cae5985b" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default))
  '(package-selected-packages
-   '(vterm-toggle cuda-mode monokai-pro-theme atom-one-dark-theme highlight-indentation pyvenv magit vterm monokai-theme undo-tree lsp-treemacs treemacs-projectile treemacs lsp-pyright counsel-projectile projectile lsp-ivy lsp-ui lsp-mode flycheck company-tabnine company rainbow-delimiters highlight-symbol dashboard marginalia which-key good-scroll smart-mode-line mwim ace-window amx counsel use-package)))
+   '(vterm-toggle cuda-mode monokai-pro-theme atom-one-dark-theme highlight-indentation pyvenv magit vterm monokai-theme undo-tree lsp-treemacs treemacs-projectile treemacs lsp-pyright counsel-projectile projectile lsp-ivy lsp-ui lsp-mode flycheck company-tabnine company rainbow-delimiters highlight-symbol dashboard marginalia which-key good-scroll smart-mode-line mwim ace-window amx counsel use-package))
+ '(vterm-toggle-fullscreen-p nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -26,11 +27,11 @@
 (eval-when-compile
   (require 'use-package))
 
-; 安装atom-one-dark主题
-(use-package atom-one-dark-theme
+; 安装monokai主题
+(use-package monokai-pro-theme
   :ensure t
   :init
-  (load-theme 'atom-one-dark 1))
+  (load-theme 'monokai-pro 1))
 
 ; 安装smart-mode-line
 (use-package smart-mode-line
