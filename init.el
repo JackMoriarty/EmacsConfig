b12c148e167de6c720461c8773799ea04b57a4a9
chenbangduo.cbd <chenbangduo.cbd@alibaba-inc.com>
2022-10-08 16:19:18 +0800

    rm beacon, replace vline with xhair

--- init.el
+++ init.el
@@ -6,14 +6,8 @@
  '(custom-safe-themes
    '("24168c7e083ca0bbc87c68d3139ef39f072488703dcdd82343b8cab71c0f62a7" "3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "171d1ae90e46978eb9c342be6658d937a83aaa45997b1d7af7657546cae5985b" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default))
  '(package-selected-packages
-   '(beacon yasnippet yasnippet-snippets vterm-toggle cuda-mode monokai-pro-theme atom-one-dark-theme highlight-indentation pyvenv magit vterm monokai-theme undo-tree lsp-treemacs treemacs-projectile treemacs lsp-pyright counsel-projectile projectile lsp-ivy lsp-ui lsp-mode flycheck company-tabnine company rainbow-delimiters highlight-symbol dashboard marginalia which-key good-scroll smart-mode-line mwim ace-window amx counsel use-package))
+   '(xhair yasnippet yasnippet-snippets vterm-toggle cuda-mode monokai-pro-theme atom-one-dark-theme highlight-indentation pyvenv magit vterm monokai-theme undo-tree lsp-treemacs treemacs-projectile treemacs lsp-pyright counsel-projectile projectile lsp-ivy lsp-ui lsp-mode flycheck company-tabnine company rainbow-delimiters highlight-symbol dashboard marginalia which-key good-scroll smart-mode-line mwim ace-window amx counsel use-package))
  '(vterm-toggle-fullscreen-p nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(vline ((t (:background "brightgreen")))))
 
 ;;; emacs 配置文件
 ;; 配置镜像
@@ -289,20 +283,14 @@
   (python-mode . highlight-indentation-mode)
   (python-mode . highlight-indentation-current-column-mode))
 
-; 滚动时高亮光标所在行
-(use-package beacon
-  :ensure t
-  :config
-  (beacon-mode t)
-  (setq beacon-size 80)
-  (setq beacon-color "#EE1F25")
-  (setq beacon-blink-duration 1.0))
-
-; 高亮当前列
+; 高亮当前行列
 (use-package vline
+  :ensure t)
+(use-package xhair
   :ensure t
   :hook
-  (prog-mode . vline-mode))
+  (prog-mode . xhair-mode))
+
 
 ;; 编辑器配置
 (electric-pair-mode t)                       ; 自动补全括号
