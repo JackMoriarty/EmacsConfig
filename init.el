8c94c1db9cb69e5a98ee751bf40416526209c77b
JackMoriarty <chenbangduo@qq.com>
2023-02-19 00:42:51 +0800

    ReConfig doom-themes and all-icons.

--- init.el
+++ init.el
@@ -34,8 +34,9 @@
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
   ;; for treemacs users
-  ;; (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
-  ;; (doom-themes-treemacs-config)
+  (if (display-graphic-p)
+      (progn (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+      (doom-themes-treemacs-config)))
   ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config))
 
