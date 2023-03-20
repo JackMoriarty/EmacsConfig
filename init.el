c0431527cc3e06284dee1c9caefa18d6b4a80c68
JackMoriarty <chenbangduo@qq.com>
2023-03-20 21:33:18 +0800

    Enable whitespace-mode

--- init.el
+++ init.el
@@ -439,13 +439,14 @@
 ;; 编辑器配置
 (add-hook 'prog-mode-hook #'electric-pair-mode)                       ;; 编程模式下自动补全括号
 (add-hook 'prog-mode-hook #'show-paren-mode)                          ;; 编程模式下, 光标在括号上时高亮另一个括号
+(add-hook 'prog-mode-hook #'whitespace-mode)                          ;; 编程模式下, 显示空格符号
+(add-hook 'prog-mode-hook #'hs-minor-mode)                            ;; 编程模式下, 可以折叠代码块
+(add-hook 'prog-mode-hook #'linum-mode)                               ;; 编程模式下, 显示行号
 (column-number-mode t)                                                ;; 在 Mode line 上显示列号
 (global-auto-revert-mode t)                                           ;; 当另一程序修改了文件时, 让 Emacs 及时刷新 Buffer
 (delete-selection-mode t)                                             ;; 选中文本后输入文本会替换文本（更符合我们习惯了的其它编辑器的逻辑）
 (setq inhibit-startup-message t)                                      ;; 关闭启动 Emacs 时的欢迎界面
 (setq make-backup-files nil)                                          ;; 关闭文件自动备份
-(add-hook 'prog-mode-hook #'hs-minor-mode)                            ;; 编程模式下, 可以折叠代码块
-(add-hook 'prog-mode-hook #'linum-mode)                               ;; 编程模式下, 显示行号
 (tool-bar-mode -1)                                                    ;; 关闭 Tool bar
 (global-set-key (kbd "M-/") 'hippie-expand)                           ;; 文本展开
 (savehist-mode 1)                                                     ;; 打开 Buffer 历史记录保存
