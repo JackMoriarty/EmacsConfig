b021a585148511b33edf1f112d6412e6be58ba1d
chenbangduo.cbd <chenbangduo.cbd@alibaba-inc.com>
2023-05-11 10:53:18 +0800

    Add nongpu package source.

--- init.el
+++ init.el
@@ -9,6 +9,7 @@
 ;;; emacs 配置文件
 ;; 配置镜像
 (setq package-archives '(("gnu"   . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+                         ("nongnu" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/nongnu/")
                          ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")))
 (package-initialize)
 
