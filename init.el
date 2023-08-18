600fe3da7fd3b928fed8cd45c14f2afab0f63f50
chenbangduo.cbd <chenbangduo.cbd@alibaba-inc.com>
2023-08-18 10:06:37 +0800

    Enable short answers.

--- init.el
+++ init.el
@@ -516,7 +516,9 @@
 (global-hl-line-mode t)                        ;; 高亮当前行
 (setq show-paren-style 'mixed)                 ;; 匹配括号高亮模式
 (setq-default indent-tabs-mode nil)            ;; 关闭tab缩进
+(setq use-short-answers t)                     ;; 使用简短的确认方式
 (setq confirm-kill-emacs 'yes-or-no-p)         ;; emacs退出前确认
+
 (defun comment-line-improve (&optional arg)
   (interactive)
   (if (not (region-active-p))
