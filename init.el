a81df1d3f761e4b566c37b9ebcfb92904e5e414d
chenbangduo.cbd <chenbangduo.cbd@alibaba-inc.com>
2025-01-17 16:04:32 +0800

    enable blamer-mode in prog-mode.

--- init.el
+++ init.el
@@ -469,6 +469,8 @@
   (blamer-face ((t :foreground "#7a88cf"
                    :background nil
                    :italic t)))
+  :hook
+  (prog-mode . blamer-mode)
   :bind
   ("<f3>" . blamer-mode))
 
