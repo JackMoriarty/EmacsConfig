1ac4fdcfe86649e956ce5dc95ffaff89056e4263
JackMoriarty <chenbangduo@qq.com>
2025-01-27 17:49:18 +0800

    add dogears.

--- init.el
+++ init.el
@@ -601,6 +601,33 @@
   :straight t
   :defer t)
 
+;; 记录光标位置
+(use-package dogears
+  :straight t
+  :hook (after-init . dogears-mode)
+  :bind (:map global-map
+              ("M-g d" . dogears-go)
+              ("M-g M-b" . dogears-back)
+              ("M-g M-f" . dogears-forward)
+              ("M-g M-d" . dogears-list)
+              ("M-g M-D" . dogears-sidebar))
+  :config
+  (setq dogears-idle 1
+        dogears-limit 200
+        dogears-position-delta 20)
+  (setq dogears-functions '(find-file recenter-top-bottom
+                                      other-window switch-to-buffer
+                                      aw-select toggle-window-split
+                                      windmove-do-window-select
+                                      pager-page-down pager-page-up
+                                      tab-bar-select-tab
+                                      pop-to-mark-command
+                                      pop-global-mark
+                                      goto-last-change
+                                      xref-go-back
+                                      xref-find-definitions
+                                      xref-find-references)))
+
 ;; 语言相关配置
 (add-hook 'prog-mode-hook #'electric-pair-mode);; 编程模式下自动补全括号
 (add-hook 'prog-mode-hook #'show-paren-mode)   ;; 编程模式下, 光标在括号上时高亮另一个括号
