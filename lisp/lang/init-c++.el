9751b448a4a05b84a992eee68c9bd2586c235fed
JackMoriarty <chenbangduo@qq.com>
2026-01-13 20:54:51 +0800

    add google-c-style for c++-mode.

--- lisp/lang/init-c++.el
+++ lisp/lang/init-c++.el
@@ -1,4 +1,8 @@
-(add-hook 'c++-mode-hook (lambda () (c-set-offset 'innamespace 0)))
+;; 安装 google-c-style 包
+(use-package google-c-style
+  :straight t
+  :hook
+  (c++-mode . google-set-c-style))
 
 (provide 'init-c++)
 ;;; init-c++.el ends here
