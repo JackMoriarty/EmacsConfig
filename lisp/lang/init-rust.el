ef21186d6055948091575a8a6c7c340f150d7478
JackMoriarty <chenbangduo@qq.com>
2026-01-24 16:05:19 +0800

    update config of rust-mode.

--- lisp/lang/init-rust.el
+++ lisp/lang/init-rust.el
@@ -1,6 +1,11 @@
 (use-package rust-mode
   :defer t
-  :straight t)
+  :straight t
+  :config
+  (setq rust-format-on-save t)
+  :hook
+  (rust-mode . (lambda () (setq indent-tabs-mode nil)))
+  (rust-mode . (lambda () (prettify-symbols-mode))))
 
 (provide 'init-rust)
 ;;; init-rust.el ends here
