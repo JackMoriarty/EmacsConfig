63c574b5c914f2108a070560143dc8c31d9987ad
JackMoriarty <chenbangduo@qq.com>
2026-03-19 19:34:23 +0800

    update corfu.

--- lisp/init-prog.el
+++ lisp/init-prog.el
@@ -21,9 +21,9 @@
   :custom
   (corfu-auto t)                 ;; enable auto completion
   (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
-  (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
-  (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
-  (corfu-preview-current nil)    ;; Disable current candidate preview
+  ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
+  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
+  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
   (corfu-preselect 'prompt)      ;; Preselect the prompt
   (corfu-on-exact-match 'insert) ;; Configure handling of exact matches
   )
