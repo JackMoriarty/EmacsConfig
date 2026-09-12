381bcb123c5cd9abe3909093cefe0deccc3a9ce0
JackMoriarty <chenbangduo@qq.com>
2026-09-13 02:09:42 +0800

    disable menu-bar.

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -154,7 +154,7 @@
        :weight 'medium)))
 
 (setq inhibit-startup-message t)               ;; 关闭启动 Emacs 时的欢迎界面
-;; (menu-bar-mode -1)                             ;; 关闭菜单栏
+(menu-bar-mode -1)                             ;; 关闭菜单栏
 (tool-bar-mode -1)                             ;; 关闭 Tool bar
 (column-number-mode t)                         ;; 在 Mode line 上显示列号
 
